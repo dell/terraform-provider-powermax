ad4e155cc87bbe68fd7c7ca62644b16786b0b00c
Shekhar Joshi <Shekhar.Joshi@dell.com>
2023-05-23 14:25:31 -0400

    Update PortGroup for acceptance test

--- examples/resources/portgroup/import.sh
+++ examples/resources/portgroup/import.sh
@@ -1,5 +1,5 @@
 # The command is
 # terraform import powermax_portgroup.portgroup_1 <id>
 # Example:
-terraform import powermax_portgroup.portgroup_1 tf_pg_test_1
+terraform import powermax_portgroup.portgroup_1 tfacc_pg_test_1
 # after running this command, populate the name field in the config file to start managing this resource
