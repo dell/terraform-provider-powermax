9f4064409644836ba551931bda15fdcd0a9566c1
Jason Jin <Jason_jin1@dell.com>
2023-05-24 02:47:28 -0700

    Update acceptance test cases to align real env (#52)

--- powermax/provider/storagegroup_data_source_test.go
+++ powermax/provider/storagegroup_data_source_test.go
@@ -8,42 +8,55 @@ import (
 )
 
 func TestAccStorageGroupDataSource(t *testing.T) {
+	var storageGroupTerraformName = "data.powermax_storagegroup.test"
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
-			// Read testing
+			// create the storage group to be tested as a data source
 			{
-				Config: ProviderConfig + testAccSgDataSourceConfig,
+				Config: ProviderConfig + StorageGroupResourceConfig,
+			},
+			// filter read testing
+			{
+				Config: ProviderConfig + StorageGroupResourceConfig + SgDataSourceConfig,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.powermax_storagegroup.test", "storage_groups.#", "1"),
-					resource.TestCheckResourceAttr("data.powermax_storagegroup.test", "storage_groups.0.name", "esa_sg572"),
-					resource.TestCheckResourceAttr("data.powermax_storagegroup.test", "storage_groups.0.id", "esa_sg572"),
-					resource.TestCheckResourceAttr("data.powermax_storagegroup.test", "storage_groups.0.device_emulation", "FBA"),
-					resource.TestCheckResourceAttr("data.powermax_storagegroup.test", "storage_groups.0.type", "Standalone"),
-					resource.TestCheckResourceAttr("data.powermax_storagegroup.test", "storage_groups.0.unprotected", "true"),
+					resource.TestCheckResourceAttr(storageGroupTerraformName, "storage_groups.#", "1"),
+					resource.TestCheckResourceAttr(storageGroupTerraformName, "storage_groups.0.name", "tfacc_sg_1"),
+					resource.TestCheckResourceAttr(storageGroupTerraformName, "storage_groups.0.id", "tfacc_sg_1"),
+					resource.TestCheckResourceAttr(storageGroupTerraformName, "storage_groups.0.srp_id", "SRP_1"),
+					resource.TestCheckResourceAttr(storageGroupTerraformName, "storage_groups.0.slo", "Gold"),
+					resource.TestCheckResourceAttr(storageGroupTerraformName, "storage_groups.0.service_level", "Gold"),
+					resource.TestCheckResourceAttr(storageGroupTerraformName, "storage_groups.0.slo_compliance", "STABLE"),
+					resource.TestCheckResourceAttr(storageGroupTerraformName, "storage_groups.0.num_of_child_sgs", "0"),
+					resource.TestCheckResourceAttr(storageGroupTerraformName, "storage_groups.0.num_of_masking_views", "0"),
+					resource.TestCheckResourceAttr(storageGroupTerraformName, "storage_groups.0.num_of_parent_sgs", "0"),
+					resource.TestCheckResourceAttr(storageGroupTerraformName, "storage_groups.0.num_of_vols", "0"),
+					resource.TestCheckResourceAttr(storageGroupTerraformName, "storage_groups.0.compression", "true"),
+					resource.TestCheckResourceAttr(storageGroupTerraformName, "storage_groups.0.unprotected", "true"),
+					resource.TestCheckResourceAttr(storageGroupTerraformName, "storage_groups.0.cap_gb", "0"),
 				),
 			},
-			// Read testing
+			// read all testing
 			{
-				Config: ProviderConfig + testAccSgAllDataSourceConfig,
+				Config: ProviderConfig + SgAllDataSourceConfig,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.powermax_storagegroup.test", "storage_groups.#", "10"),
+					resource.TestCheckResourceAttrSet("data.powermax_storagegroup.test", "storage_groups.#"),
 				),
 			},
 		},
 	})
 }
 
-const testAccSgDataSourceConfig = `
+var SgDataSourceConfig = `
 data "powermax_storagegroup" "test" {
   filter {
-    names = ["esa_sg572"]
+    names = ["tfacc_sg_1"]
   }
 }
 `
 
-const testAccSgAllDataSourceConfig = `
+var SgAllDataSourceConfig = `
 data "powermax_storagegroup" "test" {
 }
 `
