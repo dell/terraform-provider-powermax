952d6e83b63b2a2556f7fcaf70b564ddfa58742c
Nikita Joshi <131491094+nikitajoshi1@users.noreply.github.com>
2023-06-23 11:04:53 -0700

    Update masking view with OenAPI client (#74)

--- examples/resources/powermax_maskingview/resource.tf
+++ examples/resources/powermax_maskingview/resource.tf
@@ -17,8 +17,8 @@ limitations under the License.
 
 resource "powermax_maskingview" "test" {
   name             = "terraform_mv"
-  storage_group_id = "terraform_sg"
-  host_id          = "terraform_host"
+  storage_group_id = "TestnewSG"
+  host_id          = "Host124"
   host_group_id    = ""
-  port_group_id    = "terraform_pg"
+  port_group_id    = "TestnewSG_PG"
 }
